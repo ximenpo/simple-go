5d6de25d82bb52a7a7d62dc113f2b4038271327c
ximenpo <ximenpo@jiandan.ren>
2015-07-14 13:21:49 +0800

    connmgr.Conn add ReadQueue and WriteQueue.

--- connmgr/conn.go
+++ connmgr/conn.go
@@ -9,6 +9,9 @@ type Conn struct {
 	NetConn      net.Conn // 连接
 	Tag          uint32   // 标记
 	Disconnected bool     // 是否连接状态
+
+	ReadQueue  chan *Event // 读入队列，待处理，由外部设置
+	WriteQueue chan *Event // 写出队列，待发送，由外部设置
 }
 
 func (self *Conn) Close() {
